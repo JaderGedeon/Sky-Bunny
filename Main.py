a6d35e0995970fa7453703d4d90b60fb8b74db5a
Jader Gedeon <jadergedeon@gmail.com>
2020-04-24 12:35:27 -0300

    Commit Inicial

---
+++ Main.py
@@ -0,0 +1,15 @@
+"""
+Nós,
+
+// Jader Gedeon de Oliveira Rocha
+// Pedro (Dps completa teu nome...)
+
+declaramos que
+
+todas as respostas são fruto de nosso próprio trabalho,
+não copiamos respostas de colegas externos à equipe,
+não disponibilizamos nossas respostas para colegas externos à equipe e
+não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
+"""
+
+print('Hello Bunny')
